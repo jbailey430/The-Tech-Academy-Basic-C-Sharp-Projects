f321556ea5655b128415b30680a31f563905cea4
jbailey430 <60523288+jbailey430@users.noreply.github.com>
2020-07-21 20:45:40 -0700

    Commit

---
+++ AssignmentPg88/AssignmentPg88/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentPg88
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int product = 10 * 50;
+            Console.WriteLine(product);
+            Console.ReadLine();
+
+            int total = 5 + 25;
+            Console.WriteLine(total);
+            Console.ReadLine();
+
+            double quotient = 100.0 / 12.5;
+            Console.WriteLine(quotient);
+            Console.ReadLine();
+
+            bool trueOrFalse = 17 > 50;
+            Console.WriteLine(trueOrFalse);
+            Console.ReadLine();
+
+            int remainder = 143 % 7;
+            Console.WriteLine(remainder);
+            Console.ReadLine();
+        }
+    }
+}
