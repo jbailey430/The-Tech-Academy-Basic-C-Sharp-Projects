ad87544ae69ddd48f032e9dede3ca3c4c54a79a5
jbailey430 <60523288+jbailey430@users.noreply.github.com>
2020-07-21 18:25:10 -0700

    Commit

--- AssignmentPage71/AssignmentPage71/Program.cs
+++ AssignmentPage71/AssignmentPage71/Program.cs
@@ -15,19 +15,13 @@ namespace AssignmentPage71
             Console.ReadLine();
 
             Console.WriteLine("What is your name?");
-            string name = "";
-            Console.WriteLine(name);
-            Console.ReadLine();
-
-
+            string name = Console.ReadLine();
+            
             Console.WriteLine("What course are you on?");
-            string course = "";
-            Console.WriteLine(course);
-            Console.ReadLine();
+            string course = Console.ReadLine(); 
 
             Console.WriteLine("What page number?");
-            int pgNum = 0;
-            Console.ReadLine();
+            int pgNum = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Do you need help with anything? Please answer “true” or “false”.");
             bool Yes = true;
@@ -35,26 +29,16 @@ namespace AssignmentPage71
             Console.ReadLine();
 
             Console.WriteLine("Were there any positive experiences you’d like to share? Please give specifics.");
-            string comment = "";
-            Console.WriteLine(comment);
-            Console.ReadLine();
-
+            string comment = Console.ReadLine(); 
+            
             Console.WriteLine("Is there any other feedback you’d like to provide? Please be specific.");
-            string feedback = "";
-            Console.ReadLine();
-
-
+            string feedback = Console.ReadLine(); ;
+            
             Console.WriteLine("How many hours did you study today?");
-            byte hours = 0;
-            Console.ReadLine();
-
+            int hours = Convert.ToInt32(Console.ReadLine());
+            
             Console.WriteLine("Thank you for your answers. An Instructor will respond to this shortly. Have a great day!");
             Console.ReadLine();
-
-
-
-
-
 
         }
     }
