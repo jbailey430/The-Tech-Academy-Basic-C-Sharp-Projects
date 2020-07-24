caba0db24ac013bae7fba06196dec30b9efd0a45
jbailey430 <60523288+jbailey430@users.noreply.github.com>
2020-07-23 17:13:43 -0700

    Update Program.cs

--- AssignmentPg88/AssignmentPg88/Program.cs
+++ AssignmentPg88/AssignmentPg88/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Configuration;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,24 +11,41 @@ namespace AssignmentPg88
     {
         static void Main(string[] args)
         {
-            int product = 10 * 50;
-            Console.WriteLine(product);
+            Console.WriteLine("Please type a Number to times by 50");
+            int product = Convert.ToInt32(Console.ReadLine());
+            int Num = 50;
+            int combined = product * Num;
+            Console.WriteLine(combined);
             Console.ReadLine();
 
-            int total = 5 + 25;
-            Console.WriteLine(total);
+            Console.WriteLine("Please type a Number to add by 25");
+            int total = Convert.ToInt32(Console.ReadLine());
+            int Num2 = 25;
+            int plus = total + Num2;
+            Console.WriteLine(plus);
             Console.ReadLine();
 
-            double quotient = 100.0 / 12.5;
-            Console.WriteLine(quotient);
+            Console.WriteLine("Please type a Number to divide by 12.5");
+            double quotient = Convert.ToInt32(Console.ReadLine());
+            double Num3 = 12.5;
+            double all = quotient / Num3;
+            Console.WriteLine(all);
             Console.ReadLine();
 
-            bool trueOrFalse = 17 > 50;
-            Console.WriteLine(trueOrFalse);
+            Console.WriteLine("Is your Number Greater than 50?");
+            int Great = Convert.ToInt32(Console.ReadLine());
+            int currentNum = 50;
+
+            bool isGreater = Great > currentNum;
+            Console.WriteLine(isGreater);
             Console.ReadLine();
 
-            int remainder = 143 % 7;
-            Console.WriteLine(remainder);
+
+            Console.WriteLine("Please type a number to find a remainder of 7");
+            int remainder = Convert.ToInt32(Console.ReadLine());
+            int Num5 = 7;
+            int leftOver = remainder % Num5;
+            Console.WriteLine(leftOver);
             Console.ReadLine();
         }
     }
