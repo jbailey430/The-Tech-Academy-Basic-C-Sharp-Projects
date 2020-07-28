857c5c5e7fac3ebc72f0e705a9fd247891067c5a
jbailey430 <60523288+jbailey430@users.noreply.github.com>
2020-07-28 16:40:11 -0700

    commit

---
+++ AssignmentPg100/AssignmentPg100/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssignmentPg100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int Age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI?");
+            bool Dui = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int Speed = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Qualified?");
+            bool totalBool = (Age > 15 && (Dui == false  || Speed > 3));
+            Console.WriteLine(totalBool);
+            Console.ReadLine();
+        }
+    }
+}
