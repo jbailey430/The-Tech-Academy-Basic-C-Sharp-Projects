d4bd5536b265613ee83369c082d0ddeab270c2b7
jbailey430 <60523288+jbailey430@users.noreply.github.com>
2020-07-30 18:02:33 -0700

    commit

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jessie";
+            string qoute = "The man said, \"Hello\", Jesse. \n Hello on a new line. \n \t Hello on a tab.";
+            string fileName = @"C:\Users\Jesse";
+
+            bool trueOrfalse = name.Contains("s");
+
+            Console.WriteLine(trueOrfalse);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ assignmentPg125/assignmentPg125/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignmentPg125
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Jacob";
+            string lname = "Bailey";
+            string animal = "dog";
+            string total = "Hello, " + name + " " + lname + ", We didn't realize your favorite animal was a " + animal + " that is awesome!";
+
+            
+            Console.WriteLine(total);
+            Console.ReadLine();
+
+
+            name = name.ToUpper();
+            Console.WriteLine(name);
+            Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("\t My name is Jacob. My favorite animal is a dog. \n I have a dog and her name is Chloe. \n We go on walks around the neighborhood every night. \n Some days we go on hikes and even to the beach. ");
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
