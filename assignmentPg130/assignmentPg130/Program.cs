2b526f9e66ee34e398b781d35c7ab4cb6d3d3e73
jbailey430 <60523288+jbailey430@users.noreply.github.com>
2020-08-03 15:47:24 -0700

    Update Program.cs

--- assignmentPg130/assignmentPg130/Program.cs
+++ assignmentPg130/assignmentPg130/Program.cs
@@ -15,7 +15,59 @@ class Program
         numArray[3] = 4;
         numArray[4] = 5;
 
-        Console.WriteLine(numArray[]);
+        Console.WriteLine("Please enter a numer 0 through 4:");
+        int numArray1 = Convert.ToInt32(Console.ReadLine());
+        if (numArray1 < 0 || numArray1 > 4)
+        {
+            Console.WriteLine("Oops pick a number between 0 and 4!");
+        }
+        else
+        {
+            Console.WriteLine(numArray[numArray1]);
+        }
+        Console.ReadLine();
+
+
+        string[] stringArray = new string[6];
+        stringArray[0] = "Hello";
+        stringArray[1] = "my";
+        stringArray[2] = "name";
+        stringArray[3] = "is";
+        stringArray[4] = "Jake";
+
+        Console.WriteLine("Please enter a numer 0 through 4:");
+        int stringArray1 = Convert.ToInt32(Console.ReadLine());
+        if (stringArray1 < 0 || stringArray1 > 4)
+        {
+            Console.WriteLine("Oops pick a number between 0 and 4!");
+        }
+        else
+        {
+            Console.WriteLine(stringArray[stringArray1]);
+        }
+        Console.ReadLine();
+
+        List<string> intList = new List<string>();
+        intList.Add("Hello");
+        intList.Add("this");
+        intList.Add("is");
+        intList.Add("my");
+        intList.Add("list");
+
+        Console.WriteLine("Please enter a numer 0 through 4:");
+        int intList1 = Convert.ToInt32(Console.ReadLine());
+
+        if (intList1 < 0 || intList1 > 4)
+        {
+            Console.WriteLine("Oops pick a number between 0 and 4!");
+        }
+        else
+        {
+            Console.WriteLine(intList[intList1]);
+        }
+        Console.ReadLine();
+
     }
+    
 }
 
