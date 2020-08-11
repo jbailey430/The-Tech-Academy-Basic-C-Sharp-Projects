bdf0c3d2aedb5b68ef499ed631cc135645d9c8b4
jbailey430 <60523288+jbailey430@users.noreply.github.com>
2020-08-11 15:36:42 -0700

    commit

---
+++ assignmentPg161/assignmentPg161/Method.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignmentPg161
+{
+    class Method
+    {
+        public int Addition(int userInput, int Sum)
+        {
+            Sum = userInput + 12;
+            return Sum;
+        }
+        public int Multiply(int userInput, int Total)
+        {
+            Total = userInput * 10;
+            return Total;
+        }
+        public int Divide(int userInput, int sumTotal)
+        {
+            sumTotal = userInput / 2;
+            return sumTotal;
+        }
+
+    }
+
+}
---
+++ assignmentPg161/assignmentPg161/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignmentPg161
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose a number");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            Method method = new Method();
+            int Sum = 12;
+            int results = method.Addition(userInput, Sum);
+            Console.WriteLine(results);
+
+            int Total = 10;
+            int results1 = method.Multiply(userInput, Total);
+            Console.WriteLine(results1);
+
+            int sumTotal = 2;
+            int results2 = method.Divide(userInput, sumTotal);
+            Console.WriteLine(results2);
+
+            Console.ReadLine();
+        }
+    }
+}
