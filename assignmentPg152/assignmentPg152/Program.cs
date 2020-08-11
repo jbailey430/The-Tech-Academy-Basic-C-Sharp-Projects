dd59d60f58bac6efb9b1a4b03ff2bdbf0c44a79d
jbailey430 <60523288+jbailey430@users.noreply.github.com>
2020-08-11 12:20:32 -0700

    commit

---
+++ assignmentPg152/assignmentPg152/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignmentPg152
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                List<int> divide = new List<int>() { 12, 30, 10, 100 };
+                Console.WriteLine("Input a number to divide by the list");
+                int userInput = Convert.ToInt32(Console.ReadLine());
+
+                foreach (int number in divide)
+                {
+                    Console.WriteLine(number / userInput);
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Please type a whole number");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Please dont divide by Zero!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+        
+    }
+}
