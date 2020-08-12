0fe063fdfeac23c1eaaa16b8c962cd41788a3132
jbailey430 <60523288+jbailey430@users.noreply.github.com>
2020-08-12 11:01:47 -0700

    commit

---
+++ assignmentPg164/assignmentPg164/Math.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignmentPg164
+{
+    class Math
+    {
+        public int plus(int x)
+        {
+            int result = x + 5;
+            return result;
+        }
+        public int multiply(decimal x)
+        {
+            int convertX = Convert.ToInt32(x);
+            int times = convertX * 4;
+            return times;
+        }
+        public int divide(int x)
+        {
+            int total = x / 2;
+            return total;
+        }
+    }
+}
---
+++ assignmentPg164/assignmentPg164/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignmentPg164
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math mathObj = new Math();
+            Console.WriteLine("Enter a number: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            int result1 = mathObj.plus(userInput);
+            Console.WriteLine(result1);
+            Console.ReadLine();
+
+            Console.WriteLine("Enter a decimal: ");
+            decimal input = Convert.ToDecimal(Console.ReadLine());
+            int result2 = mathObj.multiply(input);
+            Console.WriteLine(result2);
+            Console.ReadLine();
+
+
+            int x;
+            Console.WriteLine("Enter a whole number: ");
+            string aValue = Console.ReadLine();
+            x = int.Parse(aValue);
+            int result3 = mathObj.divide(x);
+            Console.WriteLine(result3);
+            Console.ReadLine();
+
+        }
+
+    } 
+}
