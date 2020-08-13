7a311220200956eed1660c51e72e0e39d9603856
jbailey430 <60523288+jbailey430@users.noreply.github.com>
2020-08-13 13:29:53 -0700

    commit

---
+++ assignmentPg174/assignmentPg174/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignmentPg174
+{
+    public class Employee : Person
+    {
+        
+            public int ID { get; set; }
+    }
+}
---
+++ assignmentPg174/assignmentPg174/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignmentPg174
+{
+    public class Person
+    {
+        public List<string> Names { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        
+
+        public void SayName()
+        {
+            var FullName = FirstName + " " + LastName;
+            Console.WriteLine("Name: " + FullName);
+            Console.ReadLine();
+        }
+    }
+}
