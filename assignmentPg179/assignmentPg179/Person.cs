1768bc76c4e9a4473a3122eddd29a1923e1ea51c
jbailey430 <60523288+jbailey430@users.noreply.github.com>
2020-08-14 15:26:56 -0700

    commit

---
+++ assignmentPg179/assignmentPg179/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignmentPg179
+{
+    public abstract class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+        public void SayName()
+        {
+            var FullName = firstName + " " + lastName;
+            Console.WriteLine("Name: " + FullName);
+            Console.ReadLine();
+        }
+    }
+}
