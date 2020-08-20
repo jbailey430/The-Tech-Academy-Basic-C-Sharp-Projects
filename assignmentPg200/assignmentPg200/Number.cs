b1ad52f80a0b2c2a6d651e5c0162dbce2ad58581
jbailey430 <60523288+jbailey430@users.noreply.github.com>
2020-08-19 17:13:49 -0700

    Commit

---
+++ assignmentPg200/assignmentPg200/Number.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignmentPg200
+{
+    
+    
+        public struct Numbers
+        {
+            public decimal Amount { get; set; }
+        }
+    
+}
---
+++ assignmentPg200/assignmentPg200/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignmentPg200
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Numbers number = new Numbers() {Amount = 2.5M };
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+            
+
+        }
+    }
+    
+}
